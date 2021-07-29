f2fa2a36aed66a378bfdf10c2cd1ad96867492e8
indiofran <francisco@gomezsalaverri.com>
2021-07-29 19:03:53 -0300

    Heroku deploy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
     const express = require('express')
 const app = express()
-const port = 3030
+const port = process.env.PORT || 3000
 const path = require('path')
 
 app.get('/', function (req, res) {
